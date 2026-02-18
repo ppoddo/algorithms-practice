c36590921f558d097e63c3ec7b70d7f09cd468d2
ppoddo <your_email@example.com>
2026-02-18 21:25:52 +0900

    2026-02-18 스택/큐 관련 심화문제풀이

---
+++ programmers/42583/solution.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+
+# 트럭 여러 대가 강을 가로지르는 일차선 다리를 정해진 순으로 건너려 합니다.
+# 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다.
+# 다리에는 트럭이 최대 bridge_length대 올라갈 수 있으며, 다리는 weight 이하까지의 무게를 견딜 수 있습니다. 
+# 단, 다리에 완전히 오르지 않은 트럭의 무게는 무시합니다.
+# 예를 들어, 트럭 2대가 올라갈 수 있고 무게를 10kg까지 견디는 다리가 있습니다.
+# 무게가 [7, 4, 5, 6]kg인 트럭이 순서대로 최단 시간 안에 다리를 건너려면 다음과 같이 건너야 합니다.
+# 따라서, 모든 트럭이 다리를 지나려면 최소 8초가 걸립니다.
+# solution 함수의 매개변수로 다리에 올라갈 수 있는 트럭 수 bridge_length, 다리가 견딜 수 있는 무게 weight, 트럭 별 무게 truck_weights가 주어집니다. 
+# 이때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return 하도록 solution 함수를 완성하세요.
+
+# 제한 조건
+# bridge_length는 1 이상 10,000 이하입니다.
+# weight는 1 이상 10,000 이하입니다.
+# truck_weights의 길이는 1 이상 10,000 이하입니다.
+# 모든 트럭의 무게는 1 이상 weight 이하입니다.
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque([0] * bridge_length)
+    trucks = deque(truck_weights)
+    current_bridge_weight = 0
+    current_time = 0
+
+    while trucks or current_bridge_weight > 0:
+        current_time += 1
+        out = bridge.popleft()
+        current_bridge_weight -= out
+        if trucks and current_bridge_weight + trucks[0] <= weight:
+            t = trucks.popleft()
+            bridge.append(t)
+            current_bridge_weight += t
+        else:
+            bridge.append(0)
+
+    return current_time
+if __name__ == "__main__":
+    bridge_length = 2
+    weight = 10
+    truck_weights = [7, 4, 5, 6]
+    print(solution(bridge_length, weight, truck_weights))
